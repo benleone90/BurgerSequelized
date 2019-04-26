75926383ab414bcb313d475189d0f596a3d12a1b
Ben Leone <benleone90@users.noreply.github.com>
2019-04-25 22:39:20 -0400

    updated controller file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,7 +2,7 @@ var express = require("express");
 
 var router = express.Router();
 
-var Burgers = require("../models");
+var Burgers = require("../models/")["Burgers"];
 
 router.get("/", function(req, res) {
   Burgers.findAll({}).then(function(burger_name) {
